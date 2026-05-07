3f0ddf3ba23ef11ea59f3e862c8c340b178763ff
keeson78 <panditkishan077@gmail.com>
2026-05-07 22:02:53 +0530

    CSS JS File udpated

---
+++ index.js
@@ -0,0 +1,8 @@
+// script.js
+
+const menuIcon = document.getElementById("menuIcon");
+const navLinks = document.getElementById("navLinks");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
